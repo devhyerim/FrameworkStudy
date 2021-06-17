97f3bdf7188cae97c0e53843a20383681901d78b
devhyerimjeon <invisibleb@naver.com>
2021-06-17 18:40:56 +0900

    add mvc03 file

---
+++ mvc03/src/com/test/mvc/MemberInsertController.java
@@ -0,0 +1,57 @@
+/*==================================
+   MemberInsertController.java
+   - 사용자 정의 컨트롤러 클래스
+===================================*/
+
+package com.test.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// ※ Spring 의 'Controller' 인터페이스를 구현하는 방법을 통해
+//    사용자 정의 컨트롤러 클래스를 구현한다.
+
+public class MemberInsertController implements Controller
+{
+	private IMemberDAO dao;
+	
+	// setter 메소드 구성
+	public void setDao(IMemberDAO dao)
+	{
+		this.dao = dao;
+	}
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		ModelAndView mav = new ModelAndView();
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		// 이전 페이지로부터 넘어온 데이터 수신
+		String name = request.getParameter("name");
+		String tel = request.getParameter("telephone");
+		
+		try
+		{
+			MemberDTO member = new MemberDTO();
+			member.setName(name);
+			member.setTelephone(tel);
+			
+			dao.add(member);
+			
+		} catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+		
+		// mav.setViewName("memberlist.action 페이지를 redirect");
+		// mav.setViewName("/WEB-INF/views/MemberList.jsp"); -- (XXX)
+		mav.setViewName("redirect:memberlist.action");
+		
+		return mav;
+	}
+}
